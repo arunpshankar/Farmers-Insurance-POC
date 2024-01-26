fb75629f5e0eb2ce8313c23d6539321b66e80719
arunpshankar <arunprshankar@gmail.com>
2024-01-26 17:46:16 -0500

    wip

--- src/search/doc_search_multi_query.py
+++ src/search/doc_search_multi_query.py
@@ -1,20 +1,42 @@
-
 from src.query.expander import expand_query_and_get_variants
 from src.search.doc_search import search
 from src.config.logging import logger
-from src.config.setup import config
+from src.config.setup import *
 
-# in progress
 
-query = "How do I stop pay a refund check?"
-brand = "Farmers"
+def multi_query_search(query: str, brand: str) -> dict:
+    """
+    Expands a given query, performs a search for each variant with the specified brand, 
+    and returns the search results in a dictionary mapping each query variant to its results.
 
-variants = expand_query_and_get_variants(query)
-search_results = []
+    Args:
+    query (str): The query to be expanded and searched.
+    brand (str): The brand to be included in the search.
 
-for variant in variants:
-    result = search(query, brand)
-    search_results.append(result)
-    print(result)
-    print('-' * 100)
-#print(search_results)
+    Returns:
+    dict: A dictionary mapping each query variant to a list of search results.
+
+    Raises:
+    Exception: If an error occurs during the query expansion or search process.
+    """
+    try:
+        variants = expand_query_and_get_variants(query)
+        search_results = {}
+        result = search(query, brand)
+        search_results[query] = result
+        for variant in variants:
+            result = search(variant, brand)
+            search_results[variant] = result
+        return search_results
+    except Exception as e:
+        logger.error(f"Error in perform_brand_search with query '{query}' and brand '{brand}': {e}")
+        raise
+
+if __name__ == '__main__':
+    query = "How do I stop a refund check?"
+    brand = "Farmers"
+    results = multi_query_search(query, brand)
+    for variant, result in results.items():
+        print(f"Query: {variant}")
+        print(result)
+        print('-' * 100)
